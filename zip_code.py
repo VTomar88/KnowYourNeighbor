059fe5420bace89f4e7a619f1afbef8a1b842f70
Varsha Tomar <varshatomar88@gmail.com>
2024-05-06 19:55:28 -0400

    refactored zip_code.py for variables

--- zip_code.py
+++ zip_code.py
@@ -3,12 +3,13 @@ import pandas as pd
 from api_key import *
 
 def fetch_census_data(year, variables, zip_code):
+    # Create your own api_key.py module and store your API key from US census 
     api_key = my_key
-    
-    variables = ','.join(['DP05_0045E', 'DP05_0046E'])
+    # US Census url to fetch the data
     url = f"https://api.census.gov/data/{year}/acs/acs5/profile?get={variables}&for=zip%20code%20tabulation%20area:{zip_code}&key={api_key}"
-
+    # get the data from url
     response = requests.get(url)
+    # if the url works good, return the data
     if response.status_code == 200:
         return response.json()
     else:
@@ -17,9 +18,9 @@ def fetch_census_data(year, variables, zip_code):
 
 def main():
     # Usage example
-    # Searched from census_variables.csv. It can be extended as per the use
-    variables_dict = {'DP05_0045E': 'One race | Asian | Asian Indian', 'DP05_0046E': 'One race | Asian | Chinease'}
-    variables = list(variables_dict.keys())
+    # Searched from census_variables.csv, and can be extended to different codes
+    variables_dict = {'DP05_0044E': 'One race | Asian', 'DP05_0045E': 'One race | Asian | Asian Indian', 'DP05_0046E': 'One race | Asian | Chinease'}
+    variables = ','.join(list(variables_dict.keys()))
     year = '2022'
     zip_code = '20871'  # Replace with your desired ZIP Code
     data = fetch_census_data(year, variables, zip_code)
